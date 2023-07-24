78014dbcb15d32c05c4ed5d0a64693f397eba20f
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-07-24 15:07:47 +0200

    Updated __init__

--- barbell2_bodycomp/convert/__init__.py
+++ barbell2_bodycomp/convert/__init__.py
@@ -1,4 +1,5 @@
 from barbell2_bodycomp.convert.dcm2nifti import DicomToNifti
+from barbell2_bodycomp.convert.npy2nifti import NumpyToNifti
 from barbell2_bodycomp.convert.dcm2npy import Dicom2Numpy
 from barbell2_bodycomp.convert.npy2dcm import Numpy2Dicom
 from barbell2_bodycomp.convert.npy2nrrd import Numpy2Nrrd
