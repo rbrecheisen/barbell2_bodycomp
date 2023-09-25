827c9c53b77daaa7c0c2984ed18d46eb33cbe30e
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-09-25 12:07:30 +0200

    Saving version 0.25.0 before deploying to PyPI.

--- barbell2_bodycomp/__init__.py
+++ barbell2_bodycomp/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Ralph Brecheisen'
 __email__ = 'r.brecheisen@maastrichtuniversity.nl'
-__version__ = '0.24.0'
+__version__ = '0.25.0'
 
 from barbell2_bodycomp.calculator import BodyCompositionCalculator
 from barbell2_bodycomp.seg import MuscleFatSegmentator
